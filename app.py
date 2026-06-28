342b4da18f422493a3230b20abcf7e7189f3f1c7
Monsters806 <manaskumarndi3@gmail.com>
2026-06-28 06:54:48 +0530

    Setup LoginManager

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask, render_template
+from flask_login import LoginManager
 from config import Config
-from models import db
+from models import db, User
 from auth import auth
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.login_view = "auth.login"
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 with app.app_context():
     db.create_all()
